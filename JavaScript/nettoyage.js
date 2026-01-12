b26e41f0ec042cd2bdf5638fc6d6df7ad6e69419
Lohos06 <122732120+Lohos06@users.noreply.github.com>
2026-01-12 12:54:28 +0100

    Affichage élément par élément csv

--- JavaScript/nettoyage.js
+++ JavaScript/nettoyage.js
@@ -4,6 +4,12 @@ async function getOriginalCSV(url) {
     const Data = await Request.text();
 
     console.log(Data);
+
+    const GameArray = Data.split(";")
+
+    GameArray.forEach(Game => {
+        console.log(Game);
+    });
 }
 
 getOriginalCSV("/Data/OriginalCollection.csv");
